6620c82223f5152fdcf46e7d07c5d484def5c776
Nabil <benweb31@gmail.com>
2020-05-10 15:40:04 +0200

    first nested query

--- schema/schema.js
+++ schema/schema.js
@@ -1,19 +1,15 @@
 const graphql = require('graphql');
 const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
-const _ = require('lodash');
+const axios = require('axios');
 
-const users = [
-  {
-    id: '23',
-    firstName: 'Bill',
-    age: 20,
+const CompanyType = new GraphQLObjectType({
+  name: 'Company',
+  fields: {
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString },
   },
-  {
-    id: '47',
-    firstName: 'Samantha',
-    age: 21,
-  },
-];
+});
 
 const UserType = new GraphQLObjectType({
   name: 'User',
@@ -21,6 +17,15 @@ const UserType = new GraphQLObjectType({
     id: { type: GraphQLString },
     firstName: { type: GraphQLString },
     age: { type: GraphQLInt },
+    company: {
+      type: CompanyType,
+      resolve(parentValue, args) {
+        console.log(parentValue, args);
+        return axios
+          .get(`http://localhost:3000/companies/${parentValue.companyId}`)
+          .then((res) => res.data);
+      },
+    },
   },
 });
 
@@ -31,7 +36,7 @@ const RootQuery = new GraphQLObjectType({
       type: UserType,
       args: { id: { type: GraphQLString } },
       resolve(parentValue, args) {
-        return _.find(users, { id: args.id });
+        return axios.get(`http://localhost:3000/users/${args.id}`).then((resp) => resp.data);
       },
     },
   },
